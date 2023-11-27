0511a6dd2ec77e0a845130b94f567e0a966b697a
alex <alexdore16@gmail.com>
2023-11-27 15:38:38 +0100

    serverless

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
   <BrowserRouter basename="/Hobs-space">
     <App />
   </BrowserRouter>
